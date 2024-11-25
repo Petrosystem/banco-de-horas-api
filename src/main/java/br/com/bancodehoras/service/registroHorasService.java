622aaf99a34e0a25c3cb65b109415bce8fb88a88
marleypm16 <yarlempm16@gmail.com>
2024-11-25 16:50:00 -0300

    feat : criando serviço de registro de horas

--- src/main/java/br/com/bancodehoras/service/registroHorasService.java
+++ src/main/java/br/com/bancodehoras/service/registroHorasService.java
@@ -1,15 +1,27 @@
 package br.com.bancodehoras.service;
 
+import br.com.bancodehoras.entity.Cargos;
+import br.com.bancodehoras.entity.TipoDeRegistro;
+import br.com.bancodehoras.entity.registroHoras;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import br.com.bancodehoras.repository.registroHorasRepository;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 
 @Service
 public class registroHorasService {
 	
-	
-	
-	
-	
-	
-	
+	@Autowired
+    private registroHorasRepository registroHorasRepository;
+
+	public registroHoras save(registroHoras registroHoras){
+        registroHoras = registroHorasRepository.save(registroHoras);
+        return registroHoras;
+    }
+
+
+
 
 }
